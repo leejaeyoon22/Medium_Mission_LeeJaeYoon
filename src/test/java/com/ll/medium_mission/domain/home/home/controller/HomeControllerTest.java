cb446cb5ddd7fcba0ba9b246db5a0b0938d7c2c4
leejaeyoon22 <tksska1514@gmail.com>
2023-12-12 13:27:51 +0900

    work

---
+++ src/test/java/com/ll/medium_mission/domain/home/home/controller/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package com.ll.medium_mission.domain.home.home.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class HomeControllerTest {
+
+    @DisplayName("t1")
+    @Test
+    void t1() {
+
+    }
+
+}
